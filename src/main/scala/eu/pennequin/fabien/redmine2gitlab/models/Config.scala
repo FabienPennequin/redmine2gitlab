543dffb6ada2281bcb696465c3bfd8594cfe7212
Fabien Pennequin <fabien@pennequin.eu>
2017-04-17 11:38:07 +0200

    Convert only one project

--- src/main/scala/eu/pennequin/fabien/redmine2gitlab/models/Config.scala
+++ src/main/scala/eu/pennequin/fabien/redmine2gitlab/models/Config.scala
@@ -4,16 +4,15 @@ package models
 case class AppConfig(
   redmine: RedmineConfig,
   gitlab: GitlabConfig,
-  projects: Seq[ProjectConfig],
   users: Seq[UserConfig],
   ws: WsConfig
 )
 
-case class RedmineConfig(url: String, apiKey: String)
+case class RedmineConfig(url: String, apiKey: String, projectId: Long)
 
-case class GitlabConfig(url: String, apiKey: String)
+case class GitlabConfig(url: String, apiKey: String, projectId: Long)
 
-case class ProjectConfig(redmineId: Long, gitlabId: String)
+
 
 case class UserConfig(redmineId: Long, gitlabId: Long, gitlabKey: Option[String] = None)
 
