d389085372f3228ae6138c779799a73af6a499fb
Fabien Pennequin <fabien@pennequin.eu>
2017-07-27 08:42:42 +0200

    Fix missing import

--- src/main/scala/eu/pennequin/fabien/redmine2gitlab/services/Converter.scala
+++ src/main/scala/eu/pennequin/fabien/redmine2gitlab/services/Converter.scala
@@ -2,6 +2,8 @@ package eu.pennequin.fabien.redmine2gitlab.services
 
 import scala.concurrent.{ExecutionContext, Future}
 
+import eu.pennequin.fabien.redmine2gitlab.models.AppConfig
+
 class Converter(appConfig: AppConfig, redmine: Redmine, gitlab: Gitlab) {
 
   def run()(implicit ec: ExecutionContext) = Future.successful(())
